424f9c9d62c75171d812f62f9a875f75b2b3e43a
runordie <437043924@qq.com>
2018-01-31 15:37:20 +0800

    随意的修改，准备测试图形视图

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    mainwindow w;
-    w.show();
+   // mainwindow w;
+   // w.show();
+       QGraphicsScene *scene = new QGraphicsScene;  //场景
+       QGraphicsRectItem *item = new QGraphicsRectItem(100,100,50,50);  //矩形项
+       scene->addItem(item);  //项添加到场景
+       QGraphicsView *view = new QGraphicsView; //视图
+       view->setScene(scene);  //视图关联场景
+       view->show();  //显示视图
 
 
     return a.exec();
--- newapp.pro
+++ newapp.pro
@@ -38,5 +38,10 @@ CONFIG += mobility
 MOBILITY = 
 
 DISTFILES += \
-    template
+    template \
+    android/AndroidManifest.xml \
+    android/res/values/libs.xml \
+    android/build.gradle
+
+ANDROID_PACKAGE_SOURCE_DIR = $$PWD/android
 
